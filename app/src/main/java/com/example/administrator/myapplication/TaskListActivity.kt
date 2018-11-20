d10a037df3776569c409d62f3064a3c4b7d16999
denghuihui <908330896@qq.com>
2018-11-20 19:49:03 +0800

    增加技能

--- app/src/main/java/com/example/administrator/myapplication/TaskListActivity.kt
+++ app/src/main/java/com/example/administrator/myapplication/TaskListActivity.kt
@@ -1,12 +1,23 @@
 package com.example.administrator.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.administrator.myapplication.skill.*
 
 class TaskListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list)
+        SignInSkill().execute()
+        val shareSkill = ShareSkill()
+        shareSkill.shareNews()
+        shareSkill.listenMedia()
+        shareSkill.readNews()
+
+        WatchVideoSkill().getVideoList()
+        AudioSkill().getAudioColumn()
+        ReadNewsSkill().getMessageList()
+        LikeSkill().getLikeList()
     }
 }
